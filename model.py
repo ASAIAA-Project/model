4362043b25ba74af5aa56cc4be3a50d02d92610b
Junjie Zhang <junj.jay.zhang@gmail.com>
2022-03-29 15:52:16 -0400

    update mdoel

--- model.py
+++ model.py
@@ -6,10 +6,18 @@ import torchvision.models as models
 
 from einops import rearrange, repeat, reduce
 
+MODEL_CONFIG = {
+    
+}
 
-def create_model(args):
+
+def create_regressor(model_config):
     pass
 
+
+def create_model(model_config):
+    feature_extractor = models.feature_extraction.create_feature_extractor(regressor_trained, target_layers)
+    
 
 class ReadoutNet(nn.Module):
     def __init__(self):
@@ -20,26 +28,18 @@ class ReadoutNet(nn.Module):
         pass
 
 
-class FeatureExtractor(nn.Module):
-    def __init__(self, model, target_layers):
-        super(FeatureExtractor, self).__init__()
-        pass
-
-    def forward(self, x):
-        pass
-
-
 class Finializer(nn.Module):
-    def __init__(self):
+    def __init__(self, center_bias_weight=1):
         super(Finializer, self).__init__()
-        pass
+        self.center_bias_weight = nn.Parameter(
+            torch.Tensor([center_bias_weight]))
 
     def forward(self, x):
         pass
 
 
 class Distractor(nn.Module):
-    def __init__(self, feature_extracter, readout_net):
+    def __init__(self, feature_extracter, readout_net, finilializer):
         super(Distractor, self).__init__()
         pass
 
@@ -48,21 +48,41 @@ class Distractor(nn.Module):
 
 
 class Regressor(nn.Module):
-    def __init__(self, backbone_type='resnet18', pretrained=True):
+    def __init__(self, backbone_type='resnet18', pretrained=True, weights_path=None):
         super(Regressor, self).__init__()
         self.backbone = models.__dict__[backbone_type](pretrained=pretrained)
-        
+
         num_features = self.backbone.fc.in_features
         self.backbone.fc = nn.Linear(num_features, 2)
+        if weights_path:
+            self.backbone.load_state_dict(torch.load(weights_path))
 
     def forward(self, x):
         return self.backbone(x)
 
 
 class ASAIAANet(nn.Module):
-    def __init__(self, regressor, distractor):
+    def __init__(self, regressor, distractor, target_layer):
         super(ASAIAANet, self).__init__()
-        pass
+        regressor_extractor = []
+        regressor_predictor = []
+        
+        flag = False
+        for name, module in regressor.named_children():
+            if not flag:
+                regressor_extractor.append(module)
+            else:
+                regressor_predictor.append(module)
+            if name == target_layer:
+                flag = True
+        
+        self.regressor_extractor = nn.Sequential(*regressor_extractor)
+        self.regressor_predictor = nn.Sequential(*regressor_predictor)
+        self.distractor = distractor
 
     def forward(self, x):
-        pass
+        mask = self.regressor_extractor(x)
+        x = self.regressor_extractor(x)
+        x = x*mask + x
+        x = self.regressor_predictor(x)
+        return x
